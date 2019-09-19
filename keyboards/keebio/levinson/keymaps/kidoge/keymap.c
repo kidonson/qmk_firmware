1a74db9b2f5fea5e4c47b4252af6f5ed2b2f005b
Kidon Son <kidonson@gmail.com>
2019-09-18 20:01:29 -0400

    added gaming toggle and use layer keycodes

--- keyboards/keebio/levinson/keymaps/kidoge/keymap.c
+++ keyboards/keebio/levinson/keymaps/kidoge/keymap.c
@@ -39,10 +39,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_ortho_4x12( \
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT , \
-  KC_LCTL, KC_LGUI, KC_LALT, FUNC,    NUMS,    KC_SPC,  KC_SPC,  SYMS,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+  KC_TAB,  KC_Q,    KC_W,    KC_E,      KC_R,         KC_T,    KC_Y,    KC_U,        KC_I,    KC_O,    KC_P,    KC_BSPC, \
+  KC_ESC,  KC_A,    KC_S,    KC_D,      KC_F,         KC_G,    KC_H,    KC_J,        KC_K,    KC_L,    KC_SCLN, KC_ENT, \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,      KC_V,         KC_B,    KC_N,    KC_M,        KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT , \
+  KC_LCTL, KC_LGUI, KC_LALT, MO(_FUNC), MO(_NUMS),    KC_SPC,  KC_SPC,  MO(_SYMS),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
 /* Function 
@@ -53,14 +53,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      | Prev | Mute | Vol+ | Vol- |      |      |  F9  |  F10 |  F11 |  F12 |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |      |      |      |      |      |      |      |      |      |      |      | WASD |
  * `-----------------------------------------------------------------------------------'
  */
 [_FUNC] = LAYOUT_ortho_4x12( \
   _______, KC_PSCR, KC_INS,  KC_HOME, KC_PGUP, _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______,  \
   _______, KC_MNXT, KC_DEL,  KC_END,  KC_PGDN, _______, _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, \
   _______, KC_MPRV, KC_MUTE, KC_VOLU, KC_VOLD, _______, _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, TT(_WASD) \
 ),
 
 /* Numbers
@@ -76,8 +76,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_NUMS] = LAYOUT_ortho_4x12( \
   _______, KC_EXLM, KC_AT  , KC_HASH,  KC_DLR,   _______, _______, KC_7,    KC_8,    KC_9,    _______, _______, \
-  _______, KC_PERC, KC_CIRC, KC_AMPR,  KC_ASTR,  _______, _______, KC_5,    KC_5,    KC_6,    _______, _______, \
-  _______, _______,  KC_PLUS, KC_MINUS, KC_UNDS,  _______, _______, KC_1,    KC_2,    KC_3,    _______, _______,  \
+  _______, KC_PERC, KC_CIRC, KC_AMPR,  KC_ASTR,  _______, _______, KC_4,    KC_5,    KC_6,    _______, _______, \
+  _______, _______, KC_PLUS, KC_MINUS, KC_UNDS,  _______, _______, KC_1,    KC_2,    KC_3,    _______, _______,  \
   _______, _______, _______, _______,  _______,  _______, KC_0,    _______, _______, _______, _______, _______ \
 ),
 
@@ -107,14 +107,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |Pause |      |      |      |      |      |      |      |      |      |      |
+ * |      |Pause |      |W_FUNC|W_NUMS|      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_WASD] =  LAYOUT_ortho_4x12( \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  _______, KC_PAUS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
+  _______, _______, _______, _______,        _______,        _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______,        _______,        _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______,        _______,        _______, _______, _______, _______, _______, _______, _______, \
+  _______, KC_PAUS, _______, MO(_WASD_FUNC), MO(_WASD_NUMS), _______, _______, _______, _______, _______, _______, _______ \
 ),
 
 /* WASD Numbers
@@ -143,14 +143,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |  F7  |  F8  |  F9  |  F10 | F11  |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | F12  |      |      |      |      |      |      |      |      |      |      |
+ * |      | F12  |      |      |      |      |      |      |      |      |      | WASD |
  * `-----------------------------------------------------------------------------------'
  */
 [_WASD_FUNC] =  LAYOUT_ortho_4x12( \
   _______, KC_F1,   KC_W,    KC_F2,   KC_F3,   KC_F4,   _______, _______, _______, _______, _______, _______, \
   _______, KC_A,    KC_S,    KC_D,    KC_F5,   KC_F6,   _______, _______, _______, _______, _______, _______, \
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______, _______, _______, _______, _______, \
-  _______, KC_F12,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
+  _______, KC_F12,  _______, _______, _______, _______, _______, _______, _______, _______, _______, TT(_WASD) \
 )
 };
 
@@ -160,43 +160,5 @@ void persistent_default_layer_set(uint16_t default_layer) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case QWERTY:
-      if (record->event.pressed) {
-        persistent_default_layer_set(1UL<<_QWERTY);
-      }
-      return false;
-      break;
-    case FUNC:
-      if (record->event.pressed) {
-        layer_on(_FUNC);
-        //update_tri_layer(_LOWER, _SYMS, _GAMING);
-      } else {
-        layer_off(_FUNC);
-        //update_tri_layer(_LOWER, _SYMS, _GAMING);
-      }
-      return false;
-      break;
-    case SYMS:
-      if (record->event.pressed) {
-        layer_on(_SYMS);
-        //update_tri_layer(_LOWER, _SYMS, _GAMING);
-      } else {
-        layer_off(_SYMS);
-        //update_tri_layer(_LOWER, _SYMS, _GAMING);
-      }
-      return false;
-      break;
-    case NUMS:
-      if (record->event.pressed) {
-        layer_on(_NUMS);
-        //update_tri_layer(_LOWER, _SYMS, _GAMING);
-      } else {
-        layer_off(_NUMS);
-        //update_tri_layer(_LOWER, _SYMS, _GAMING);
-      }
-      return false;
-      break;
- }
   return true;
 }
